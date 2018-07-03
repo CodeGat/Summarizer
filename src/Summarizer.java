edd3625e98fa9fa6d25fe43efcbe02a5269a15cb
Tommy Phoenix Gatti <u6044453@anu.edu.au>
2018-07-03 22:29:56 +0200

    TLDR: Added command line args

--- src/Summarizer.java
+++ src/Summarizer.java
@@ -1,19 +1,38 @@
+import Significance.SignificantSentences;
+import Significance.SignificantWords;
+import WebCrawler.Crawler;
+
 import java.util.*;
 
 public class Summarizer {
-    private final static int N_SIG_WORDS     = 5;
-    private final static int N_SIG_SENTENCES = 5;
+    private static int N_SIG_WORDS     = 5;
+    private static int N_SIG_SENTENCES = 5;
 
     public static void main(String[] args) {
-        // TODO: crawl for web stories, instead just use system.in for now like the pleb you are.
         // TODO: 27/06/2018 prefers long sentences
         // TODO: 27/06/2018 sentence parsing - doesn't like regex(*U.S. *)
-        Scanner in = new Scanner (System.in);
-        ArrayList<String> raw_corpus = new ArrayList<>();
-        while (in.hasNext()) raw_corpus.add(in.nextLine().toLowerCase());
-        String corpus = raw_corpus
-                .stream()
-                .reduce("", (a, b) -> a+b);
+        String corpus = "";
+        switch (args.length) {
+            case 3:
+                N_SIG_WORDS = Integer.valueOf(args[0]);
+                N_SIG_SENTENCES = Integer.valueOf(args[1]);
+                corpus = Crawler.crawl(args[2]);
+                break;
+            case 1:
+                corpus = Crawler.crawl(args[0]);
+                break;
+            case 0:
+                Scanner in = new Scanner(System.in);
+                System.out.print("> ");
+                corpus = Crawler.crawl(in.next());
+                break;
+            default:
+                System.out.println("Usage: Summarizer.java [[n_sig_words n_sig_sentences] url]\nwhere:\n\tn_sig_words - " +
+                        "number of significant words to use to determine a significant sentence\n\tn_sig_sentences - " +
+                        "number of significant sentences to return\n\turl - the url of the paper or story");
+                System.exit(0);
+        }
+        System.out.println(corpus);
 
         String[] sentences     = corpus.split("\\.");
         String[] words         = corpus.split("[ .,]");
